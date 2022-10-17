0c1c87d45f2bf70545b462122ef025c8fff91f8c
sunnybaghla <sunnybaghla@rocketmail.com>
2022-10-17 19:57:45 +0530

    fin

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const options={
     cert:fs.readFileSync("server.cert")
 }
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true }).then(()=>{
     console.log('db connected');
 }).catch((e)=>{
     console.log(e);
